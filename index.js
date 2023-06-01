c6da2364bd770830d17b4fa8ebf40d2053517ed1
Emmanuel Adesile <emmanueladesile@Emmanuels-MacBook-Pro-2.local>
2023-06-01 14:25:25 +0100

    setup database

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
-const bootcampRoutes = require('./routes/bootcamps');
+import morgan from "morgan";
+import colors from "colors";
 
-dotenv.config({
-  path: "./config/config.env",
-});
+import bootcampRoutes from "./routes/bootcamps";
+import connectToDb from "./config/dbConnect";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-// routes
-app.use('/api/v1/bootcamps', bootcampRoutes);
+// db Connection
+connectToDb();
 
-app.listen(PORT, () => console.log(`Server is running on PORT - ${PORT}`));
+// middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// routes
+app.use("/api/v1/bootcamps", bootcampRoutes);
+
+const server = app.listen(PORT, () =>
+  console.log(`Server is running on PORT - ${PORT}`.yellow.bold)
+);
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  server.close(() => process.exit(1));
+});
